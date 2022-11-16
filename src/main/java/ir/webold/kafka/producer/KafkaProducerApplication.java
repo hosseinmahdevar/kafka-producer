89566a8fbea958036792e455f118d8d515c4d829
hossein <mahdevarhossein@gmail.com>
2022-11-16 15:05:52 +0330

    json push

--- src/main/java/ir/webold/kafka/producer/KafkaProducerApplication.java
+++ src/main/java/ir/webold/kafka/producer/KafkaProducerApplication.java
@@ -1,22 +1,56 @@
 package ir.webold.kafka.producer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @SpringBootApplication
 public class KafkaProducerApplication {
-
-	public static void main(String[] args) {
+	public final DateFormat dateFormat =new SimpleDateFormat("YYYY/MM/dd hh:mm:ss");
+	    @Value("${kafka.topic.name}")
+	    private String topicName;
+		public static void main(String[] args) {
 		SpringApplication.run(KafkaProducerApplication.class, args);
 	}
 	@Bean
-	CommandLineRunner commandLineRunner(KafkaTemplate<String,String> template){
+	CommandLineRunner commandLineRunner(KafkaTemplate<String,SamplePojo> template){
 		return args -> {
-			template.send("sampleTopic","salam ma ");
+			template.send(topicName,new SamplePojo("hossein","mahdevar"));
+
 		};
+	}
+
+	public static class SamplePojo{
+			private String name;
+			private String family;
+
+		public SamplePojo(String name, String family) {
+			this.name = name;
+			this.family = family;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getFamily() {
+			return family;
+		}
+
+		public void setFamily(String family) {
+			this.family = family;
+		}
 	}
 
 }
--- src/main/java/ir/webold/kafka/producer/config/KafkaProducerConfig.java
+++ src/main/java/ir/webold/kafka/producer/config/KafkaProducerConfig.java
@@ -1,6 +1,7 @@
 package ir.webold.kafka.producer.config;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ser.impl.UnknownSerializer;
+import ir.webold.kafka.producer.KafkaProducerApplication;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,16 +22,17 @@ public class KafkaProducerConfig {
     public Map<String,Object> props(){
         Map<String,Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
         return props;
     }
     @Bean
-    public ProducerFactory<String,String> producerFactory(){
+    public ProducerFactory<String, KafkaProducerApplication.SamplePojo> producerFactory(){
         return new DefaultKafkaProducerFactory<>(props());
     }
     @Bean
-    public KafkaTemplate<String,String> kafkaTemplate(ProducerFactory<String,String> producerFactory){
+    public KafkaTemplate<String, KafkaProducerApplication.SamplePojo
+            > kafkaTemplate(ProducerFactory<String, KafkaProducerApplication.SamplePojo> producerFactory){
         return new KafkaTemplate<>(producerFactory);
     }
 }
